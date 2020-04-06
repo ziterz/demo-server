c249a20c334c68caf9c368c88e706901491d2743
Ziady Mubaraq <ziady.mubaraq@gmail.com>
2020-04-06 14:41:38 +0700

    update config

--- models/index.js
+++ models/index.js
@@ -9,8 +9,8 @@ const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
 let sequelize;
-if (config["use_env_variable"]) {
-  sequelize = new Sequelize(config["use_env_variable"], {
+if (process.env[config.use_env_variable]) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
     dialect: "postgres",
     protocol: "postgres"
   });
